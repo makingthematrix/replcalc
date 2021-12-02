e7328bf30fa8421ec730c8df10cfe386d39065fc
Maciej Gorywoda <makingthematrix@gmail.com>
2021-12-02 22:30:27 +0100

    Basic expressions, unparsed text, constants
    https://github.com/makingthematrix/replcalc/projects/1#card-74101657
    
    "Expression" is the basic node for thr mini-AST.
    For now it has two subtypes. "Text" contains the unparsed text expression, and "Constant" is parsed from it.
    I use NaN if the text can't be parsed.

---
+++ src/test/scala/replcalc/eval/ExpressionTest.scala
@@ -0,0 +1,14 @@
+package replcalc.eval
+
+import munit.Location
+
+class ExpressionTest extends munit.FunSuite:
+  implicit val location: Location = Location.empty
+
+  test("Number") {
+    assertEqualsDouble(Expression("4").evaluate, 4.0, 0.001)
+    assertEqualsDouble(Expression("-3").evaluate, -3.0, 0.001)
+    assertEqualsDouble(Expression("4.12").evaluate, 4.12, 0.001)
+    assertEqualsDouble(Expression("0").evaluate, 0.0, 0.00001)
+    assertEqualsDouble(Expression("blah").evaluate, Double.NaN, 0.1)
+  }
