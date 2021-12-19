542cb68db6b9a9edad2a1e6d390f88aaa73053b8
Maciej Gorywoda <makingthematrix@gmail.com>
2021-12-19 22:47:54 +0100

    refactor: Check if the parser is set at the beginning of preprocessing
    Makes for more readable code.
    Also a few cosmetic changes in other files.

--- src/main/scala/replcalc/expressions/Failure.scala
+++ src/main/scala/replcalc/expressions/Failure.scala
@@ -5,4 +5,3 @@ import replcalc.Parser
 object Failure extends Parseable[Expression]:
   override def parse(parser: Parser, line: String): ParsedExpr[Expression] = 
     ParsedExpr.error(s"Unable to parse: $line")
-
